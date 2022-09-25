61f6cff399331a178f10c7dfcdc711f704929085
MarceloSardo <marc.sardo1940@gmail.com>
2022-09-25 00:17:13 -0300

    Projeto oneBit

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculateTip(event) {
+    event.preventDefault();
+
+    let bill = document.getElementById('bill').value;
+    let servQual = document.getElementById('servQual').value;
+    let numOfPeople = document.getElementById('people').value;
+
+    if(bill == '' | servQual == 0){
+        alert("Preencha os valores!")
+        return;
+    }
+
+    if(numOfPeople == "" | numOfPeople <= 1){
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "none"
+    } else{
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (bill * servQual) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = "block";
+}
+
+document.getElementById('totalTip').style.display="none";
+document.getElementById('each').style.display="none";
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
